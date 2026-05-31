817352bdb3efe13367161ff5fd0b2ecd1d81f77a
daveli <lchwt123@gmail.com>
2026-05-31 23:39:17 +0800

    chore: bump to v1.0.0.60

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -414,7 +414,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                 children: [
                   Text('IG Downloader', overflow: TextOverflow.ellipsis),
                   Text(
-                    'v1.0.0.59',
+                    'v1.0.0.60',
                     style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400),
                   ),
                 ],
